28a753f1f953d6805832867a2494d92440053de7
dtanajewski <tanajewski@wp.pl>
2016-03-22 12:08:39 +0100

    Added files via upload

---
+++ pesel.py
@@ -0,0 +1,25 @@
+class PeselValidate:
+    'Klasa sprawdzająca poprawnośc wprowadzonego numeru PESEL'
+
+    def __init__(self, str1):
+        self.PESEL=str1
+
+    def display_info(self):
+        PESEL_list=[]
+        for i in str(self.PESEL):
+            PESEL_list.append(int(i))
+        control_sum = 1*PESEL_list[0] + 3*PESEL_list[1] + 7*PESEL_list[2] + 9*PESEL_list[3] + 1*PESEL_list[4] + 3*PESEL_list[5] + 7*PESEL_list[6] + 9*PESEL_list[7] + 1*PESEL_list[8] + 3*PESEL_list[9]
+        r=control_sum%10
+        control_number=0
+        if r==0:
+            control_number=0
+        else:
+            control_number=10-r
+        if control_number==PESEL_list[10]:
+            return 'PESEL prawidlowy'
+        else:
+            return 'PESEL nieprawidlowy'
+
+PESEL_input=input("Podaj nr PESEL: ")
+p1=PeselValidate(PESEL_input)
+print(p1.display_info())
