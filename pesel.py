2be9a5a6faf83ec0bc7144cbb5f377c46fb3f444
dtanajewski <tanajewski@wp.pl>
2016-03-22 12:24:01 +0100

    Update pesel.py

--- pesel.py
+++ pesel.py
@@ -10,7 +10,6 @@ class PeselValidate:
             PESEL_list.append(int(i))
         control_sum = 1*PESEL_list[0] + 3*PESEL_list[1] + 7*PESEL_list[2] + 9*PESEL_list[3] + 1*PESEL_list[4] + 3*PESEL_list[5] + 7*PESEL_list[6] + 9*PESEL_list[7] + 1*PESEL_list[8] + 3*PESEL_list[9]
         r=control_sum%10
-        control_number=0
         if r==0:
             control_number=0
         else:
@@ -18,7 +17,7 @@ class PeselValidate:
         
         return control_number==PESEL_list[10]
 
-
+###test
 PESEL_input=input("Podaj nr PESEL: ")
 p1=PeselValidate(PESEL_input)
 print(p1.validate())
